6e0ad836e62e8a3366b81ee1935f2046bb1452ee
khh5762 <khh5762@naver.com>
2023-03-13 17:52:57 +0900

    Fix: instanceof에 LoginUser 확인 후 자동 주입

--- src/main/java/shop/mtcoding/aopstudy/handler/LoginAdvice.java
+++ src/main/java/shop/mtcoding/aopstudy/handler/LoginAdvice.java
@@ -1,11 +1,15 @@
 package shop.mtcoding.aopstudy.handler;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
@@ -19,23 +23,23 @@ public class LoginAdvice {
 
     @Around("execution(* shop.mtcoding.aopstudy.controller..*.*(..))")
     public Object loginUserAdvice(ProceedingJoinPoint jp) throws Throwable {
-        Object result = jp.proceed();
-        // Object[] param = new Object[10];
         Object[] args = jp.getArgs();
-        System.out.println(result.toString());
-        
-        for(Object arg : args){
-            if(arg instanceof LoginUser){
-                HttpServletRequest req = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
-                HttpSession session = req.getSession();
-                
-                User principal = (User) session.getAttribute("principal");
-                args[0] = principal;
-                System.out.println("테스트 : "+args[0]);
-                result = jp.proceed(args);
+        MethodSignature signature = (MethodSignature) jp.getSignature();
+        Method method = signature.getMethod();
+        Annotation[][] annotationsPA = method.getParameterAnnotations();
+
+        for (int i = 0; i < args.length; i++) {
+            Annotation[] annotations = annotationsPA[i]; // 첫번째 파라메터의 어노테이션, 두번째 파라메터의 어노테이션
+            for (Annotation anno : annotations) {
+                if (anno instanceof LoginUser) {
+                    HttpServletRequest req = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+                    HttpSession session = req.getSession();
+                    User principal = (User) session.getAttribute("principal");
+                    return jp.proceed(new Object[]{principal});
+                }
             }
         }
 
-        return result;
+        return jp.proceed();
     }
 }
